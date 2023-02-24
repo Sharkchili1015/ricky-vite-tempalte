a9db7b96544be8f329e2a7410aae161dc71f5146
Ricky <1372896443@qq.com>
2023-02-24 23:35:40 +0800

    feat:base glob auto router

--- src/router/glob.ts
+++ src/router/glob.ts
@@ -6,7 +6,7 @@ function getModules() {
   return components
 }
 
-function letterToLowerCase(str: string,) {
+function letterToLowerCase(str: string) {
   return str[0].toLowerCase() + str.slice(1)
 }
 
@@ -15,16 +15,16 @@ export const vueRouters = function (): Array<RouteRecordRaw> {
   const routerList: Array<RouteRecordRaw> = []
   const modules = getModules()
   Object.keys(modules).forEach((key) => {
-    console.log('🚀 ~ file: glob.ts:29 ~ Object.keys ~ key:', key)
     const path = key.replace('../pages', '').replace('/index.vue', '')
-    console.log('🚀 ~ file: glob.ts:23 ~ Object.keys ~ path:', path)
     const name = path.replace('/', '')
+    console.log('modules[key]', modules[key])
     // 首字母转小写 letterToLowerCase 首字母转大写 letterToUpperCase
     routerList.push({
       path: `${letterToLowerCase(path)}`,
       name: `${letterToLowerCase(name)}`,
       component: modules[key],
     })
+    console.log('🚀 ~ file: glob.ts:29 ~ Object.keys ~ routerList:', routerList)
   })
   return routerList
 }
